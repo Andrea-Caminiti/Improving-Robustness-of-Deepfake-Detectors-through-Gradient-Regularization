4612acff0223f1277ef2681d58111ec3adea8896
Andrea-Caminiti <caminiti.1924053@studenti.uniroma1.it>
2025-05-08 23:45:28 +0200

    Removed unused parameters

--- Train/train.py
+++ Train/train.py
@@ -8,70 +8,69 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc
 from Network.model import CBAMEfficientNet
 from Train.loss import GradientRegularizedLoss
 
-def train(dLoader_train, dLoader_valid, epochs, save_path, log_path, patience, loss_fn = nn.BCEWithLogitsLoss(), device = 'cuda'):
+def train(dLoader_train, dLoader_valid, epochs, patience, loss_fn = nn.BCEWithLogitsLoss(), device = 'cuda'):
     model = CBAMEfficientNet().to(device)
     optimizer = torch.optim.Adam(model.parameters(True))
     gradientRegularized = isinstance(loss_fn, GradientRegularizedLoss)
     losses = {'Train': [], 'Valid': []}
     metrics = {'Train': {'accuracy': [], 'precision': [], 'recall': [], 'f-score': [], 'auc': []},
                'Valid': {'accuracy': [], 'precision': [], 'recall': [], 'f-score': [], 'auc': []}}
-    with open(log_path, 'w') as log:
-        for epoch in range(epochs):
-            model.train()
-            epoch_loss = []        
-            preds, gt = [], []
-            best_valid_loss = 0.0
-            for batch in tqdm(dLoader_train, desc=f'Epoch {epoch}: Training...', total=len(dLoader_train)):
-                optimizer.zero_grad()
+    for epoch in range(epochs):
+        model.train()
+        epoch_loss = []        
+        preds, gt = [], []
+        best_valid_loss = 0.0
+        for batch in tqdm(dLoader_train, desc=f'Epoch {epoch}: Training...', total=len(dLoader_train)):
+            optimizer.zero_grad()
 
-                imgs, labels = batch
-                imgs = imgs.to(device)
-                labels = labels.to(device)
+            imgs, labels = batch
+            imgs = imgs.to(device)
+            labels = labels.to(device)
 
-                logits, shallow_feat = model(imgs)
+            logits, shallow_feat = model(imgs)
 
-                if gradientRegularized:
-                    loss = loss_fn(model, logits, shallow_feat, labels)
-                else: 
-                    loss = loss_fn(logits, labels)
-
-                loss.backward()
-                optimizer.step()
-                epoch_loss.append(loss.item())
-                preds.append(logits.argmax(dim=1).cpu())
-                gt.append(labels.cpu())
-
-            losses['Train'].append(np.mean(epoch_loss).item())
-            metrics['Train']['accuracy'].append(accuracy_score(gt, preds))
-            prec, rec, f, _ = precision_recall_fscore_support(gt, preds, average='macro')
-            auc = roc_auc_score(gt, preds)
-            metrics['Train']['precision'].append(prec)
-            metrics['Train']['recall'].append(rec)
-            metrics['Train']['f-score'].append(f)
-            metrics['Train']['auc'].append(auc)
-            l, acc, prec, rec, f, auc = valid(model, dLoader_valid, epoch, device)
-            losses['Valid'].append(l)
-            metrics['Valid']['accuracy'].append(acc)
-            metrics['Valid']['precision'].append(prec)
-            metrics['Valid']['recall'].append(rec)
-            metrics['Valid']['f-score'].append(f)
-            metrics['Valid']['auc'].append(auc)
-
-            if best_valid_loss == 0.0 or l < best_valid_loss:
-                #Save the most accurate model 
-                best_valid_loss = l
-                if not os.path.exists(r'./models'):
-                    os.mkdir(r'./models')
-                torch.save(model, rf'./models/{"Gradient Regularized" if gradientRegularized else "Baseline"} at epoch {epoch}.pt')
-                
-            if l > best_valid_loss and patience_c == patience:
-                    break
-                    
-            elif l > best_valid_loss:
-                patience_c += 1
-            
+            if gradientRegularized:
+                loss = loss_fn(model, logits, shallow_feat, labels)
             else: 
-                patience_c = 0
+                loss = loss_fn(logits, labels)
+
+            loss.backward()
+            optimizer.step()
+            epoch_loss.append(loss.item())
+            preds.append(logits.argmax(dim=1).cpu())
+            gt.append(labels.cpu())
+
+        losses['Train'].append(np.mean(epoch_loss).item())
+        metrics['Train']['accuracy'].append(accuracy_score(gt, preds))
+        prec, rec, f, _ = precision_recall_fscore_support(gt, preds, average='macro')
+        auc = roc_auc_score(gt, preds)
+        metrics['Train']['precision'].append(prec)
+        metrics['Train']['recall'].append(rec)
+        metrics['Train']['f-score'].append(f)
+        metrics['Train']['auc'].append(auc)
+        l, acc, prec, rec, f, auc = valid(model, dLoader_valid, epoch, device)
+        losses['Valid'].append(l)
+        metrics['Valid']['accuracy'].append(acc)
+        metrics['Valid']['precision'].append(prec)
+        metrics['Valid']['recall'].append(rec)
+        metrics['Valid']['f-score'].append(f)
+        metrics['Valid']['auc'].append(auc)
+
+        if best_valid_loss == 0.0 or l < best_valid_loss:
+            #Save the most accurate model 
+            best_valid_loss = l
+            if not os.path.exists(r'./models'):
+                os.mkdir(r'./models')
+            torch.save(model, rf'./models/{"Gradient Regularized" if gradientRegularized else "Baseline"} at epoch {epoch}.pt')
+            
+        if l > best_valid_loss and patience_c == patience:
+                break
+                
+        elif l > best_valid_loss:
+            patience_c += 1
+        
+        else: 
+            patience_c = 0
 
     losses = pd.DataFrame(losses)
     metrics_t = pd.DataFrame(metrics['Train'])
