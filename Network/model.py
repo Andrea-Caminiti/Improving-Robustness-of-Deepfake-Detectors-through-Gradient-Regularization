be441e82db474584164142cd7c36bcc8fe8a9e45
Andrea-Caminiti <caminiti.1924053@studenti.uniroma1.it>
2025-05-08 11:52:18 +0200

    Small changes to model division in sequential blocks to beter work with loss

--- Network/model.py
+++ Network/model.py
@@ -7,18 +7,17 @@ class CBAMEfficientNet(nn.Module):
     def __init__(self, num_classes=2):
         super(CBAMEfficientNet, self).__init__()
         # Stem
-        self.stem = nn.Sequential(
+        self.shallow = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False), #299x299 -> 150x150
             nn.BatchNorm2d(32),
-            nn.SiLU()
-        )
-        
-        # MBConv blocks: (in, out, stride)
-        self.blocks = nn.Sequential(
+            nn.SiLU(),
             MBConvBlock(32, 16, kernel_size = 3, stride=1), #150x150 -> 75x75
 
             MBConvBlock(16, 24, kernel_size = 3, stride=2), #75x75 -> 75x75
             MBConvBlock(24, 24, kernel_size = 3, stride=1), #75x75 -> 75x75
+        )
+        
+        self.deep = nn.Sequential(
 
             MBConvBlock(24, 40, kernel_size=5, stride=2, padding=2), #75x75 -> 38x38
             MBConvBlock(40, 40, kernel_size=5, stride=1, padding=2), #38x38 -> 38x38
@@ -37,21 +36,21 @@ class CBAMEfficientNet(nn.Module):
             MBConvBlock(192, 192, kernel_size=5, stride=1, padding=2), #5x5 -> 5x5
 
             MBConvBlock(192, 320, kernel_size=5, stride=1), #5x5 -> 1x1
+            nn.Conv2d(320, 1280, 1),
+            nn.AdaptiveAvgPool2d(1),
         )
         
         # Head
-        self.head = nn.Sequential(
-            nn.Conv2d(320, 1280, 1),
-            nn.AdaptiveAvgPool2d(1),
+        self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(1280, num_classes)
         )
 
     def forward(self, x):
-        x = self.stem(x)
-        x = self.blocks(x)
-        x = self.head(x)
-        return x
+        shallow = self.shallow(x)
+        x = self.deep(shallow)
+        x = self.classifier(x)
+        return x, shallow
     
 if __name__ == '__main__':
     model = CBAMEfficientNet()
