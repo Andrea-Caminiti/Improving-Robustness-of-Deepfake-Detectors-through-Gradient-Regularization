d0a5e67a0d839207beb2d00e28bd0930081a0858
Andrea-Caminiti <caminiti.1924053@studenti.uniroma1.it>
2025-05-10 18:15:04 +0200

    Implemented global constants and main function to call everything already implemented

---
+++ globals.py
@@ -0,0 +1,21 @@
+import torch
+
+class Config:
+    def __init__(self):
+        #Log Paths
+        self.logPath_baseline = 'logs/baseline.txt'
+        self.logPath_regularized = 'logs/regularized.txt'
+        self.logPath_attack_baseline = 'logs/adversarial_baseline.txt'
+        self.logPath_attack_regularized = 'logs/adversarial_regularized.txt'
+        #Device
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        #Data Paths
+        self.train_dataPath = 'CV Dataset\Train'
+        self.valid_dataPath = 'CV Dataset\Validtion'
+        self.test_dataPath = 'CV Dataset\Test'
+        #Loss parameters
+        self.r = 0.05
+        self.alpha = 0.75
+        #Model Paths
+        self.best_model_normal = ''
+        self.best_model_regularized = ''
---
+++ main.py
@@ -0,0 +1,38 @@
+import torch
+import gc
+import numpy as np
+from Network.model import CBAMEfficientNet
+from Train.train import train
+from Train.loss import GradientRegularizedLoss
+from Evaluation.test import test, plots
+from Data.dataset import create_dataset
+from globals import Config
+
+def main():
+    config = Config()
+    print(f'Using {config.device}')
+    #Train Baseline
+    train(config.train_dataPath, config.valid_dataPath, epochs=50, patience=5, device=config.device)
+    #Train Gradient Regularized model
+    train(config.train_dataPath, config.valid_dataPath, epochs=50, patience=5, loss_fn=GradientRegularizedLoss(config.r, config.alpha), device=config.device)
+    
+    model = CBAMEfficientNet(2)
+    model.load_state_dict(config.best_model_normal)
+    #Test Baseline
+    test(model, config.test_dataPath, device=config.device, log_path=config.logPath_baseline)
+    del model
+    gc.collect()
+    model = CBAMEfficientNet(2)
+    model.load_state_dict(config.best_model_regularized)
+    #Test Regularized model
+    test(model, config.test_dataPath, device=config.device, log_path=config.logPath_regularized)
+
+
+if __name__ == '__main__':
+    seed = 3233
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    main()
